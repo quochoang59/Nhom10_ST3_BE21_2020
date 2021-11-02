2353015c17410c26cf5fb18775e85840085722ec
doanquochuy <huy123s1s1qwe@gmail.com>
2021-11-02 09:27:37 +0700

    first commit

---
+++ models/product.php
@@ -0,0 +1,31 @@
+<?php
+class Product extends Db
+{
+    public function getAllProducts()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products order by id desc limit 0,5");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an
+    }
+    public function getProductById($id)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE id = ?");
+        $sql->bind_param("i", $id);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an
+    }
+    public function search($keyword)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE `name` like ?");
+        $keyword = "%$keyword%";
+        $sql->bind_param("s", $keyword);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an
+    }
+}
